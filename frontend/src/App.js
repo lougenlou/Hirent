42d212bf8a62e3642c834ee4940caea6ee2d3ac6
Maria Curly Ann A. Lumibao <mlumibao@gbox.adnu.edu.ph>
2025-11-06 00:00:39 +0800

    fix app.js:duplication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -24,7 +24,6 @@ import OwnerDashboard from "./pages/owner/OwnerDashboard";
 import AddItem from "./pages/owner/AddItem";
 import OwnerProfile from "./pages/owner/OwnerProfile";
 
-import OwnerInformation from "./pages/owners/Owner_Information";
 import OwnerSubmission from "./pages/owners/Owner_Submission";
 import Onboarding1 from "./pages/onboarding/Onboarding1";
 import Onboarding2 from "./pages/onboarding/Onboarding2";

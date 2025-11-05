a247ff59421ee6b4afebaf3c75c36ae9c72867a2
Maria Curly Ann A. Lumibao <mlumibao@gbox.adnu.edu.ph>
2025-11-05 23:57:49 +0800

    fix index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,44 +1,15 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import "./assets/Auth.css";
-=======
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-=======
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
->>>>>>> fb5b9551f50f26dd278c1ec027b4cfebe2206411
 import App from "./App";
 import "@fontsource/inter/400.css";
 import "@fontsource/inter/500.css";
 import "@fontsource/inter/700.css";
-<<<<<<< HEAD
-=======
->>>>>>> origin
->>>>>>> fb5b9551f50f26dd278c1ec027b4cfebe2206411
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-<<<<<<< HEAD
-  React.createElement(
-    React.StrictMode,
-    null,
-    React.createElement(App)
-  )
-);
-=======
   <React.StrictMode>
     <App />
   </React.StrictMode>
-<<<<<<< HEAD
 );
-=======
-);
->>>>>>> origin
->>>>>>> fb5b9551f50f26dd278c1ec027b4cfebe2206411

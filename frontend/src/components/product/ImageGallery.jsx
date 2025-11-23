185bebb7636199b8eeb7700e23acb5fbacb3efd5
Hazel <hasalvador@gbox.adnu.edu.ph>
2025-11-24 00:35:34 +0800

    added details product

--- frontend/src/components/product/ImageGallery.jsx
+++ frontend/src/components/product/ImageGallery.jsx
@@ -14,62 +14,66 @@ const ImageGallery = ({ images }) => {
 
   return (
     <div className="flex gap-4">
-      
+
       {/* Thumbnails */}
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-3">
         {images.map((img, i) => (
           <button
             key={i}
             onClick={() => setSelectedImage(i)}
-            className={`w-20 h-20 p-2 rounded-lg border flex items-center justify-center cursor-pointer
+            className={`w-16 h-16 p-1.5 rounded-xl border flex items-center justify-center cursor-pointer transition
               ${
                 selectedImage === i
                   ? "border-purple-600 shadow-md"
                   : "border-gray-300 hover:border-purple-400"
               }`}
           >
-            <img src={img} alt="" className="object-contain w-full h-full" />
+            <img
+              src={img}
+              alt=""
+              className="object-contain w-full h-full rounded-md"
+            />
           </button>
         ))}
       </div>
 
-      {/* Main Image Box */}
-      <div className="relative flex-1 bg-white border rounded-xl shadow-sm flex items-center justify-center p-6">
+      {/* Main Image */}
+      <div className="relative flex-1 bg-white border rounded-xl shadow-sm flex items-center justify-center p-3">
 
         {/* Prev Arrow */}
         <button
           onClick={prevImage}
-          className="absolute left-4 top-1/2 -translate-y-1/2 bg-white border shadow-md rounded-full p-2 hover:bg-purple-600 hover:text-white transition"
+          className="absolute left-2 top-1/2 -translate-y-1/2 bg-white border shadow-md rounded-full p-1.5 hover:bg-purple-600 hover:text-white transition"
         >
-          <ChevronLeft className="w-5 h-5" />
+          <ChevronLeft className="w-4 h-4" />
         </button>
 
         <img
           src={images[selectedImage]}
-          className="w-full max-h-[480px] object-contain"
+          className="w-full max-h-[300px] object-contain"
           alt="product"
         />
 
         {/* Next Arrow */}
         <button
           onClick={nextImage}
-          className="absolute right-4 top-1/2 -translate-y-1/2 bg-white border shadow-md rounded-full p-2 hover:bg-purple-600 hover:text-white transition"
+          className="absolute right-2 top-1/2 -translate-y-1/2 bg-white border shadow-md rounded-full p-1.5 hover:bg-purple-600 hover:text-white transition"
         >
-          <ChevronRight className="w-5 h-5" />
+          <ChevronRight className="w-4 h-4" />
         </button>
 
         {/* Dots */}
-        <div className="absolute bottom-4 left-1/2 -translate-x-1/2 flex gap-2">
+        <div className="absolute bottom-3 left-1/2 -translate-x-1/2 flex gap-2">
           {images.map((_, i) => (
             <button
               key={i}
               onClick={() => setSelectedImage(i)}
               className={`h-2 rounded-full transition-all
-              ${
-                selectedImage === i
-                  ? "bg-gray-900 w-5"
-                  : "bg-gray-400 w-2 hover:bg-gray-600"
-              }`}
+                ${
+                  selectedImage === i
+                    ? "bg-gray-900 w-5"
+                    : "bg-gray-400 w-2 hover:bg-gray-600"
+                }`}
             ></button>
           ))}
         </div>

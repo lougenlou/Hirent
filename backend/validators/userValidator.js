8dd9d5c98613da5bb7d04b72cb9d94192ebec47c
Maria Curly Ann A. Lumibao <mlumibao@gbox.adnu.edu.ph>
2025-11-25 15:44:49 +0800

    userValidator.js -const

--- backend/validators/userValidator.js
+++ backend/validators/userValidator.js
@@ -1,4 +1,4 @@
-export const validateUser = (req, res, next) => {
+const validateUser = (req, res, next) => {
     const { username, email, password } = req.body;
 
     if (!username || typeof username !== 'string') {
@@ -6,6 +6,6 @@ export const validateUser = (req, res, next) => {
     }
 
     next();
-}
+};
 
 module.exports = { validateUser };

27ae976bf82efdc7d01b76fc3c87e847fbe8aae5
Hazel <hasalvador@gbox.adnu.edu.ph>
2025-11-28 21:00:46 +0800

    Owner Earnings Dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import OwnerDashboard from './pages/owner/OwnerDashboard';
-import AddItem from './pages/owner/AddItem';
-import OwnerProfile from './pages/owner/OwnerProfile';
-import MyListings from './pages/owner/MyListings';
+// Owner Pages
+import OwnerDashboard from "./pages/owner/OwnerDashboard";
+import AddItem from "./pages/owner/AddItem";
+import OwnerProfile from "./pages/owner/OwnerProfile";
+import MyListings from "./pages/owner/MyListings";
+import OwnerEarnings from "./pages/owner/OwnerEarnings"; // ⭐ New Earnings Page
 
 function App() {
   return (
@@ -15,6 +17,9 @@ function App() {
         <Route path="/owner/my-listings" element={<MyListings />} />
         <Route path="/owner/add-item" element={<AddItem />} />
         <Route path="/owner/profile" element={<OwnerProfile />} />
+
+        {/* ⭐ NEW: Earnings Page */}
+        <Route path="/owner/earnings" element={<OwnerEarnings />} />
 
         {/* Optional redirect */}
         <Route path="/owner" element={<OwnerDashboard />} />

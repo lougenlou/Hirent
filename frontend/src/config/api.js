dedc1063c5d0681eeaec88c61ac9c4f8724549ae
Maria Curly Ann A. Lumibao <mlumibao@gbox.adnu.edu.ph>
2025-12-11 08:45:10 +0800

    final na

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -4,7 +4,7 @@
 
 // Base API URL
 export const API_URL =
-  process.env.REACT_APP_API_URL || "http://localhost:5000";
+  process.env.REACT_APP_FRONTEND_URL || "http://localhost:5000";
 
 // IMPORTANT — all endpoints must start with /api
 const API_PREFIX = "/api";

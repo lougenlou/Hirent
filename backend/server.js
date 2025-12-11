871efd28685caeb6a6950259b6ce352ec4c11d8e
Maria Curly Ann A. Lumibao <mlumibao@gbox.adnu.edu.ph>
2025-12-11 08:20:23 +0800

    backend domainnnn

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ const app = express();
 // Middleware
 // -------------------------
 app.use(cors({
-  origin: "http://localhost:3001",
+  origin: "https://hirenttttttt.netlify.app",
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
